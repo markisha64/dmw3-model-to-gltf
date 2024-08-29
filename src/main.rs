263b2480e4a8212642d46df61c8da6160797d7f9
Marko Grizelj <marko.grizelj@gauss.dev>
2024-08-29 14:09:07 +0200

    refact: moved to util

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod pack;
 
 use binread::{io::Cursor, BinRead};
 use clap::Parser;
+use dmw3_model::{AnimationFrame, AnimationFrames, AnimationInst, Header, Part};
 use gltf_json as json;
 use image::RgbaImage;
 use json::material::{EmissiveFactor, PbrBaseColorFactor, PbrMetallicRoughness, StrengthFactor};
@@ -26,53 +27,6 @@ struct Args {
 
     #[arg(short, long, default_value_t = 1)]
     threads: usize,
-}
-
-#[derive(BinRead)]
-struct PartPacked {
-    parent_index: u32,
-    geometry: u32,
-    animation: u32,
-}
-
-#[derive(BinRead)]
-struct AnimationInst {
-    t: u16,
-    len: u16,
-    c: u16,
-    d: u16,
-}
-
-#[derive(BinRead, Debug, Clone)]
-struct AnimationFrame {
-    vx: i16,
-    vy: i16,
-    vz: i16,
-    id: u16,
-}
-
-#[derive(Debug, Clone)]
-struct AnimationFrames {
-    translation: AnimationFrame,
-    rotation: AnimationFrame,
-    scale: AnimationFrame,
-}
-
-#[derive(Clone, Debug)]
-struct Part {
-    vert_offset: usize,
-    vert_len: usize,
-    tex_offset: usize,
-    tex_len: usize,
-    _animation: usize,
-}
-
-#[derive(BinRead)]
-struct Header {
-    texture_offset: u32,
-    _part_count: u32,
-    #[br(count=_part_count)]
-    parts: Vec<PartPacked>,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -1480,6 +1434,8 @@ fn process_file(path: &PathBuf, header_index: Option<usize>) -> anyhow::Result<(
     let mut header_reader = Cursor::new(&header_raw);
 
     let header = Header::read(&mut header_reader)?;
+
+    dbg!(&header.texture_offset);
 
     let filename_os = match path.file_stem() {
         Some(x) => x,
